080709384763cb65c3538432f788bffe97db8d76
tatigalle23 <tatigalle23@gmail.com>
2021-11-05 00:57:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let cactusMov=setInterval(function() {
         cactusRight += 1;
         cactus.style.right = `${cactusRight}px`
-    },10); 
+    },5); 
 
-    setInterval(function(){
+    /*setInterval(function(){
     const dino = document.querySelector('.dino');
     let dinoUp = 21;    
     let dinoJump=setInterval(function() {
@@ -26,6 +26,41 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 10);
         setTimeout(() => {clearInterval(dinoJumpDown);}, 1000);
     }, 1000);
-    },2000);   
+    },2000);*/
+    
+    const dino = document.querySelector('.dino');
+    let bottom=21
+    let gravity=0.9
+    let isJumping= false
+    function jump(){
+        if(isJumping) return 
+        let timerUpId= setInterval(function(){
+            if(bottom>150){
+                clearInterval(timerUpId)
+                let timerDownId=setInterval(function(){
+                    if(bottom<21){
+                        clearInterval(timerDownId)
+                        isJumping=false
+                    }
+                    bottom -=5
+                    //bottom=bottom*gravity
+                    dino.style.bottom=bottom+'px'
+                },30)
+            }
+            isJumping=false
+            bottom+=40
+            bottom=bottom*gravity
+            dino.style.bottom=bottom+'px'
+        },30)
+        
+    }
+    
+
+    function control(e){
+        if(e.keyCode==38 || e.keyCode==32){
+            jump()
+        }
+    }
+    document.addEventListener('keydown',control)
 
 });
