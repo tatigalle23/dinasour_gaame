1ffdc316bd92acdf2baba6e57bd453e750592853
tatigalle23 <tatigalle23@gmail.com>
2021-11-09 23:11:01 +0900

    update

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
+    var count=0 
 
-    
+    const score= document.querySelector('.count');
     const cactus = document.querySelector('.cactus');
-    let cactusRight = 50;    
+    let cactusRight = 0;    
     let cactusMov=setInterval(function() {
+        let cactusAgain=setInterval(function(){
+            if(cactusRight>1400){
+                cactusRight=0;
+            }
+        },3);
         cactusRight += 1;
         cactus.style.right = `${cactusRight}px`
-    },5); 
-
-    /*setInterval(function(){
-    const dino = document.querySelector('.dino');
-    let dinoUp = 21;    
-    let dinoJump=setInterval(function() {
-        dinoUp += 1;
-        dino.style.bottom = `${dinoUp}px`
+        count+=1;
+        score.textContent("SCORE: "+count);
         
-    }, 10);
-    setTimeout(() => {clearInterval(dinoJump);}, 1000);
-    setTimeout(() => {
-        let dinoDown = 120;
-        let dinoJumpDown=setInterval(function() {
-            dinoDown -= 1;
-            dino.style.bottom= `${dinoDown}px`
-            
-        }, 10);
-        setTimeout(() => {clearInterval(dinoJumpDown);}, 1000);
-    }, 1000);
-    },2000);*/
+                
+    },3);  
+    
+
+   
+
+    const cactus3= document.querySelector('.cactus3');
+    let cactusRight3 = -500;    
+    let cactusMov3=setInterval(function() {
+        let cactusAgain=setInterval(function(){
+            if(cactusRight3>1400){
+                cactusRight3=-500;
+            }
+        },3);
+        cactusRight3 += 1;
+        cactus3.style.right = `${cactusRight3}px`
+    },3);
+
     
     const dino = document.querySelector('.dino');
     let bottom=21
@@ -43,24 +49,33 @@ document.addEventListener("DOMContentLoaded", () => {
                         isJumping=false
                     }
                     bottom -=5
-                    //bottom=bottom*gravity
                     dino.style.bottom=bottom+'px'
                 },30)
             }
-            isJumping=false
+            isJumping=true
             bottom+=40
             bottom=bottom*gravity
             dino.style.bottom=bottom+'px'
         },30)
         
-    }
-    
+    }    
 
     function control(e){
-        if(e.keyCode==38 || e.keyCode==32){
+        if(e.keyCode===38 || e.keyCode===32){
             jump()
         }
     }
     document.addEventListener('keydown',control)
+
+    let stopGame=setInterval(function(){
+        let cactusLeft=cactus.getClientRects()[0].left
+        let cactusLeft1=cactus3.getClientRects()[0].left
+
+        let dinoTop=dino.getClientRects()[0].bottom
+        console.log(dinoTop)
+        if(cactusLeft<80 && cactusLeft>0 && dinoTop>=467 || cactusLeft1<80 && cactusLeft1>0 && dinoTop>=467){
+            alert("Game over");
+        }
+    },10);
 
 });
